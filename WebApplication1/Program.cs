71a8ee868155258e2bd8f1fdf78ded35089a22a3
AnVDB <anvdb_tts@rikkeisoft.com>
2024-10-08 09:47:58 +0700

    added comment

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -17,7 +17,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
+// asdfasdf
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
