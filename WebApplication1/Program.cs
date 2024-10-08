c9a3b9d3beada861a4183f98f7e162e97b064994
AnVDB <anvdb_tts@rikkeisoft.com>
2024-10-08 09:45:35 +0700

    change comment

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -17,7 +17,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 
-//new line 
+//conflict line
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
